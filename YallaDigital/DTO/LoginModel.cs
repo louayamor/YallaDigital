d69edf0d5b388ba926960a45a75138b947111f69
louayamor <amor.louay20@gmail.com>
2025-04-18 21:40:20 +0100

    feature/Added Remember me

--- YallaDigital/DTO/LoginModel.cs
+++ YallaDigital/DTO/LoginModel.cs
@@ -4,9 +4,13 @@ using System.ComponentModel.DataAnnotations;
 
 public class LoginModel
 {
-    [Required, EmailAddress]
-    public string Email { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
 
     [Required]
-    public string Password { get; set; }
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+
+    public bool RememberMe { get; set; }
 }
