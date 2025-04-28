97863e28000ed77fc67122fea91eea62d79bc8ba
louayamor <amor.louay20@gmail.com>
2025-04-28 18:57:52 +0100

    Added admin dashboard template

---
+++ YallaDigital/Areas/Admin/Views/Dashboard/index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YallaDigital.Areas.Admin.Views.Dashboard;
+
+public class index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ YallaDigital/Areas/Admin/Views/Shared/_layout.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YallaDigital.Areas.Admin.Views.Shared;
+
+public class _layout : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
